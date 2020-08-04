c71721547cb7d7dd77678d670c4a9b0824f1bff6
kiang <kiange@gmail.com>
2020-08-04 23:01:43 +0800

    force using 4326 in outSR

--- 01_fetch.php
+++ 01_fetch.php
@@ -11,7 +11,7 @@ while(++$lastId) {
     $targetFile = __DIR__ . '/raw/data_' . $lastId . '.json';
     if(!file_exists($targetFile)) {
       $q = implode(',', $objects);
-      $json = shell_exec("curl -k 'https://map.coa.gov.tw/portal/sharing/servers/bdfbd5edc5c243ff929d61853997d58f/rest/services/Farmland_survey/L21_%E6%96%B0%E5%A2%9E%E5%B7%A5%E5%BB%A0106Q4/MapServer/0/query?objectIds={$q}&outFields=*&returnGeometry=true&f=json' -H 'Host: map.coa.gov.tw' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0' -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Referer: https://map.coa.gov.tw/farmland/survey.html' -H 'Connection: keep-alive'");
+      $json = shell_exec("curl -k 'https://map.coa.gov.tw/portal/sharing/servers/bdfbd5edc5c243ff929d61853997d58f/rest/services/Farmland_survey/L21_%E6%96%B0%E5%A2%9E%E5%B7%A5%E5%BB%A0106Q4/MapServer/0/query?objectIds={$q}&outFields=*&outSR=4326&returnGeometry=true&f=json' -H 'Host: map.coa.gov.tw' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0' -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Referer: https://map.coa.gov.tw/farmland/survey.html' -H 'Connection: keep-alive'");
       $json = gzdecode($json);
       $obj = json_decode($json, true);
       if(isset($obj['features'][0])) {
