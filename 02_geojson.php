416bfea27263fa2c5a124fb65ec63eb228fdb392
kiang <kiange@gmail.com>
2020-08-04 23:39:12 +0800

    generate factories.csv

--- 02_geojson.php
+++ 02_geojson.php
@@ -3,6 +3,7 @@ $fc = array(
     'type' => 'FeatureCollection',
     'features' => array(),
 );
+$addr = array();
 foreach(glob(__DIR__ . '/raw/*.json') AS $jsonFile) {
     $json = json_decode(file_get_contents($jsonFile), true);
     foreach($json['features'] AS $f) {
@@ -16,7 +17,16 @@ foreach(glob(__DIR__ . '/raw/*.json') AS $jsonFile) {
                 'coordinates' => $f['geometry']['rings'],
             ),
         );
+        $addr[$f['attributes']['標準地籍址']] = true;
         $fc['features'][] = $gf;
     }
 }
 file_put_contents(__DIR__ . '/factories.json', json_encode($fc));
+
+ksort($addr);
+$addrList = array_keys($addr);
+$fh = fopen(__DIR__ . '/factories.csv', 'w');
+fputcsv($fh, array('住址'));
+foreach($addrList AS $addr) {
+    fputcsv($fh, array($addr));
+}
